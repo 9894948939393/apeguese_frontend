3e43911d7757c296d6299eec612a6d6a2afa86b3
Ana Laura <analauracnunes@icloud.com>
2025-06-01 21:06:13 -0300

    commit 2

---
+++ frontend/src/Admin.js
@@ -0,0 +1,325 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import './Admin.css';
+import Main from './main.js';
+import App from './App.js';
+function Admin() {
+    const [mostarPedidos, setMostrarPedidos] = useState(true);
+    const [criar, setCriar] = useState(false);
+    const [editar, setEditar] = useState(false);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [produtos, setProdutos] = useState([]);
+    const [pedidos, setPedidos] = useState([]);
+    
+  
+  useEffect(() => {     
+          if (location.pathname === '/admin') {
+            setMostrarPedidos(true)
+          } else{
+            setCriar(false)
+            setEditar(false)
+            setMostrarPedidos(false)
+          }
+        }, [location.pathname]);
+  
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Dados recebidos da API Flask:', data);
+        })
+        .catch(error => {
+          console.error('Erro ao buscar dados da API Flask:', error);
+        });
+    }, []);
+    useEffect(() => {
+      fetch('http://127.0.0.1:5001')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Dados recebidos da API Flask:', data);
+        })
+        .catch(error => {
+          console.error('Erro ao buscar dados da API Flask:', error);
+        });
+    }, []);
+  
+    const adicionarProduto = (event) => {
+      event.preventDefault(); 
+  
+      const formData = new FormData(event.target);
+  
+      fetch('http://127.0.0.1:5001/adicionar_produto', {
+        method: 'POST',
+        body: formData,
+        mode: 'cors',
+        credentials: 'include'
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Resposta do logar:', data);
+          if (data && data.message === "Produto adicionado com sucesso!") {
+            alert(data.message)
+            fetch("/produtos")
+          } else if (data && data.message === "Não foi possível criar produto, tente novamente") {
+            alert(data.message);
+          }else {
+            alert("Erro desconhecido ao adicionar.");
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao adicionar tipo:', error);
+          alert('Erro ao adicio tipo: ' + error.message);
+        });
+    };
+  
+    const atualizarPreco = (event) => {
+      event.preventDefault(); 
+  
+      const formData = new FormData(event.target);
+  
+      fetch('http://127.0.0.1:5001/atualizar_preco', {
+        method: 'POST',
+        body: formData,
+        mode: 'cors',
+        credentials: 'include'
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Resposta do logar:', data);
+          if (data && data.message === "Valor atualizado com sucesso!") {
+            alert(data.message)
+          } else if (data && data.message === "Não foi possível atualizar o valor, tente novamente") {
+            alert(data.message);
+          }else {
+            alert("Erro desconhecido ao atualizar.");
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao atualizar tipo:', error);
+          alert('Erro ao atualizar tipo: ' + error.message);
+        });
+    };
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:5001/produtos') 
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          setProdutos(data.produtos);
+        })
+        .catch(error => {
+          console.error('Erro ao buscar produtos:', error);
+        });
+    }, []);
+    useEffect(() => {
+        fetch('http://127.0.0.1:5001/pedidos') 
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then(data => {
+            setPedidos(data.pedidos);
+          })
+          .catch(error => {
+            console.error('Erro ao buscar produtos:', error);
+          });
+      }, []);
+    const deletarProduto = (event) => {
+      event.preventDefault(); 
+  
+      const formData = new FormData(event.target);
+  
+      fetch('http://127.0.0.1:5001/deletar_produto', {
+        method: 'POST',
+        body: formData,
+        mode: 'cors',
+        credentials: 'include'
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Resposta do logar:', data);
+          if (data && data.message === "Produto deletado com sucesso") {
+            alert(data.message)
+            fetch("/produtos")
+          } else if (data && data.message === "Não foi possível deletar o produto, tente novamente") {
+            alert(data.message);
+          }else {
+            alert("Erro desconhecido ao atualizar.");
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao atualizar tipo:', error);
+          alert('Erro ao atualizar tipo: ' + error.message);
+        });
+    };
+    const navegarParaLogin = () => {
+      navigate('/app');
+    };
+  
+    const irPedidos = () => {
+      setMostrarPedidos(true)
+      setCriar(false)
+      setEditar(false)
+      };
+      const irCriar = () => {
+        setMostrarPedidos(false)
+        setCriar(true)
+        setEditar(false)
+        };
+        const irEditar = () => {
+          setMostrarPedidos(false)
+          setCriar(false)
+          setEditar(true)
+          };
+    return (
+      <main>
+        <div style={{display: mostarPedidos? 'flex' : 'none' }}>
+        {pedidos.map(filtro => (
+            <div key={filtro[0]} className='divProduto'>   
+                    <h1>{filtro[3]}</h1>
+                    <h3>Pagante:{filtro[2]}</h3>
+                    <p>Usuário:{filtro[1]}</p>
+                    <p>Valor: {filtro[4]}</p>
+                    <p>Telefone: {filtro[6]}</p>
+                    <p>Endereço: {filtro[7]}</p>
+                    <p>Status: {filtro[5]}</p>
+                    <form method="post">
+                        Alterar status pedido:
+                        <select name="status" id="">
+                            <option value="Preparando pedido">Preperando pedido</option>
+                            <option value="Enviado">Enviado</option>
+                            <option value="Entregue">Entregue</option>
+                            <option value="Cancelado">Cancelado</option>
+                        </select>
+                    </form>
+                </div>
+        ))}  
+        </div>
+        <div style={{display: criar? 'flex' : 'none' }} className='divCriar'>
+          <div>
+          <form  onSubmit={adicionarProduto}method="post">
+              <h3>Criar produto</h3>
+              <br />
+              <label>Produto:</label>
+              <br />
+              <input type='text' name="nome" placeholder='Digite aqui o produto que deseja criar' />
+              <br />
+              <label>Marca:</label>
+              <br />
+              <input type='text' name="marca" placeholder='Digite aqui a marca' />
+              <br/>
+              <label>Cor:</label>
+              <br />
+              <input type='text' name="cor" placeholder='Digite aqui a cor' />
+              <br />
+              <label>Numeração:</label>
+              <br />
+              <input type='number' step="0.01" name="numeracao" placeholder='Digite aqui o preço' />
+              <br />
+              <br></br>
+              <select name="genero">
+                <option value="" disabled>Selecione o gênero</option>
+                <option value="masculino">Masculino</option>
+                <option value="feminino">Feminino</option>
+                <option value="unissex">Unissex</option>
+              </select>
+              <br />
+              <label>Descrição:</label>
+              <br />
+              <input type='text' name="descricao" placeholder='Digite aqui a descrição do produto' />
+              <br />
+              <label>Preço:</label>
+              <br />
+              <input type='number' step="0.01" name="valor" placeholder='Digite aqui o preço' />
+              <br />
+              <label>Imagem:</label>
+              <br />
+              <input type='file' name="imagem" />
+              <button type='submit' style={{color:"white"}}>Criar</button>
+              <br />
+            </form>
+          </div>
+        </div>
+        <div style={{display: editar? 'flex' : 'none' }} className='divEditar'>
+          <div> 
+              <form onSubmit={atualizarPreco} method="post">
+                  <h3>Atualizar o preço</h3>
+                  <label>Novo preço:</label>
+                  <br />
+                  <input type='number' name="preco" placeholder='Digite aqui o novo preço' />
+                  <br />
+                  <label>Filial:</label>
+                  <br />           
+                  <label>Produto:</label>
+                  <br />
+                  <select name="produto" id="">
+                  <option value="">Selecione o produto</option>
+                  {produtos.map(produto => (
+                      <option key={produto[0]} value={produto[1]}>{produto[1]}</option>
+                  ))}
+                  </select>
+                  <button type='submit'>atualizar</button>
+              </form>
+          </div>
+          <div> 
+              <form onSubmit={deletarProduto} method="post">
+                  <h3>Deletar produto</h3>           
+                  <label>Produto:</label>
+                  <br />
+                  <select name="produto" id="">
+                  <option value="">Selecione o produto</option>
+                  {produtos.map(produto => (
+                      <option key={produto[0]} value={produto[1]}>{produto[1]}</option>
+                  ))}
+                  </select>
+                  <button type='submit'>Deletar</button>
+              </form>
+          </div>
+        </div>
+        <div className='barraAdmin'>
+          <button onClick={irCriar}><img src="/images/criar.png" alt='adicionar'></img></button>
+          <button onClick={irEditar}><img src="/images/editar.png" alt='editar'></img></button>
+          <button onClick={irPedidos}><img src="/images/pedidos.png" alt='pedido'></img></button>              
+        </div>
+        <Routes>
+          <Route path="/app" element={<App />} />
+        </Routes>
+      </main>
+    );
+  }
+  
+  
+  export default Admin;
---
+++ frontend/src/Perfil.js
@@ -0,0 +1,116 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import './Perfil.css';
+import Main from './main.js';
+function Perfil() {
+const [mensagem, setMensagem] = useState('');
+const [perfil, setPerfil] = useState('');
+const [mostrarGeralPerfil, setMostrarGeralPerfil] = useState(true);
+const navigate = useNavigate();
+const location = useLocation();
+  
+useEffect(() => {     
+    if (location.pathname === '/perfil') {
+      setMostrarGeralPerfil(true)
+      carregarPerfil();
+    } else{
+      setMostrarGeralPerfil(false)
+    }
+  }, [location.pathname]);
+
+useEffect(() => {
+    axios.get('http://localhost:5000/api/hello')//conexão com o backend
+      .then(res => setMensagem(res.data.message))
+      .catch(err => console.error(err));
+  }, []);
+  const navegarParaMain = () => {
+    navigate('/main');
+  };
+  const carregarPerfil = () => {
+    fetch('http://localhost:5000/perfil', {
+      method: "GET",
+      credentials: 'include'
+    }) 
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.perfil && data.perfil.length > 0) {
+          setPerfil(data.perfil[0]);
+        } else {
+          setPerfil(null);
+        }
+      })
+      .catch(error => {
+        console.error('Erro ao buscar perfil:', error);
+      });
+  };
+
+  return (
+    <main>
+      <header style={{display: mostrarGeralPerfil? 'flex' : 'none'}}>
+      <div className='headerContainer'>
+          <img src="/images/logo_dark.png" alt='club raro'></img>
+          <form>
+            <input type="search" placeholder='Pesquisar perfil'></input>
+            <button type='submit'>
+              <img src='/images/lupa.png' alt='lupa'></img>
+            </button>
+          </form>
+          <div className='divBotoes'>
+              <button ><img alt='Carrinho' src='/images/carrinho.png'></img></button>
+              <button onClick={navegarParaMain}> <img  alt='Perfil' src='/images/home.png'></img></button>
+          </div>
+        </div>
+      </header>
+      <div>
+        <div style={{display: mostrarGeralPerfil? 'flex' : 'none',color:"white", flexDirection:"column"}}>
+          <h1>Sua conta</h1>
+          <div style={{display:"flex"}}>
+          <div>
+            <button>Alterar email</button>
+            <button>Alterar telefone</button>
+            <button>Alterar nome de usuário</button>
+            <button>Alterar senha</button>
+            <button>Alterar endereço</button>
+          </div>
+          {perfil ?(
+              <div key={perfil[0]} className='divInformacoesPerfil'>
+                <h3>Nome de usuário</h3>                
+                <p>{perfil[1]}</p>
+                <h3>Email</h3>                
+                <p>{perfil[2]}</p>
+                <h3>Telefone</h3>                
+                <p>{perfil[4]}</p>
+                <h3>Cep</h3>                
+                <p>{perfil[11]}</p>
+                <h3>Rua</h3>                
+                <p>{perfil[13]}</p>
+                <h3>Número</h3>                
+                <p>{perfil[12]}</p>
+                <h3>Bairro</h3>                
+                <p>{perfil[14]}</p>
+                <h3>Cidade</h3>                
+                <p>{perfil[15]}</p>
+                <h3>Estado</h3>                
+                <p>{perfil[16]}</p>
+                <h3>Complemento</h3>                
+                <p>{perfil[17]}</p>
+              </div>
+            ):( 
+              <p>Carregando perfil...</p>)}
+          </div>
+        </div>
+      </div>
+      <footer style={{display: mostrarGeralPerfil? 'flex' : 'none'}}></footer>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </main>
+  );
+}
+export default Perfil;
