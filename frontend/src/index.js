04780d65d95551527edcac5ec5b09dd9ae159a52
Ana Laura <analauracnunes@icloud.com>
2025-06-04 10:15:13 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';
+import AppWrapper from './AppWrapper';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+    <AppWrapper />
+  </BrowserRouter>
   </React.StrictMode>
 );
 
